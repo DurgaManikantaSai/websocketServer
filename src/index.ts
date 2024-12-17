aca3b657a5aae44c8894077e8e9a1cf5e0e43a76
= <y.d.manikantasai@gmail.com>
2024-12-17 15:31:54 +0530

    websocketServer Created

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import WebSocket, {WebSocketServer} from "ws";
+import http from 'http';
+
+const server = http.createServer((req:any,res:any) => {
+    console.log(`${new Date()} + received request for ${req.url}`);
+    res.end("Hi server");
+})
+
+const wss = new WebSocketServer({server});
+
+wss.on('connection', function connection(socket){
+    socket.on('error', console.error);
+
+    socket.on('message', function message(data, isBinary){
+        wss.clients.forEach(function each(client){
+            if(client.readyState === WebSocket.OPEN){
+                client.send(data,{binary: isBinary});
+            }
+        });
+    });
+    socket.send("Hello! Message from Server");
+})
+
+server.listen(8080, () => {
+    console.log((new Date()) + 'Server is listening on port 8080');
+})
